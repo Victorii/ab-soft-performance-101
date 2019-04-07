79802c47e37c996d0c40a410c59ab9408e5657ab
victoria.maziar <victoria.maziar@ab-soft.net>
2019-04-07 20:35:30 +0300

    add report

--- homework/lc4/gatling-test/src/test/scala/ods/vm/gatling/LoadSimulation_run.scala
+++ homework/lc4/gatling-test/src/test/scala/ods/vm/gatling/LoadSimulation_run.scala
@@ -3,12 +3,13 @@ package ods.vm.gatling
 import io.gatling.core.scenario.Simulation
 import ods.vm.gatling.scenarios.{CartScenario, HPScenario, ProductScenario}
 import io.gatling.core.Predef._
-
-
 /**
-  * Created by Victoria on 06.04.2019.
+  * Created by Victoria on 07.04.2019.
+  *
+  * The report is based on a run this class.
   */
-class LoadSimulation extends Simulation{
+
+class LoadSimulation_run extends Simulation{
 
   val scenario1 = scenario("Users: HP -> all product -> product details -> add to cart -> view cart -> checkout")
     .exec(HPScenario.openHomePage)
@@ -17,7 +18,7 @@ class LoadSimulation extends Simulation{
     .exec(CartScenario.addProductToCart)
     .exec(CartScenario.viewCart)
     .exec(CartScenario.checkout)
-    .inject(rampUsers(60) during 3600)
+    .inject(rampUsers(3) during 180) //rampUsers(60) during 3600
 
 
   val scenario2  = scenario("Users: HP -> all product -> product details -> all product -> product details")
@@ -26,7 +27,7 @@ class LoadSimulation extends Simulation{
     .exec(ProductScenario.viewProductDetails)
     .exec(ProductScenario.viewAllProducts)
     .exec(ProductScenario.viewProductDetails)
-    .inject(rampUsers(400) during 3600)
+    .inject(rampUsers(20) during 180) //rampUsers(400) during 3600
 
 
   val scenario3  = scenario("Users: HP -> all product -> product details -> add to cart -> product details")
@@ -35,7 +36,7 @@ class LoadSimulation extends Simulation{
     .exec(ProductScenario.viewProductDetails)
     .exec(CartScenario.addProductToCart)
     .exec(ProductScenario.viewProductDetails)
-    .inject(rampUsers(220) during 3600)
+    .inject(rampUsers(11) during 180) //rampUsers(220) during 3600
 
   val scenario4  = scenario("Users: HP -> all product -> product details -> add to cart -> product details -> view cart -> checkout")
     .exec(HPScenario.openHomePage)
@@ -45,21 +46,21 @@ class LoadSimulation extends Simulation{
     .exec(ProductScenario.viewProductDetails)
     .exec(CartScenario.viewCart)
     .exec(CartScenario.checkout)
-    .inject(rampUsers(20) during 3600)
+    .inject(rampUsers(1) during 180) //rampUsers(20) during 3600
 
   val scenario5  = scenario("Users: HP -> all product -> product details -> add to cart")
     .exec(HPScenario.openHomePage)
     .exec(ProductScenario.viewAllProducts)
     .exec(ProductScenario.viewProductDetails)
     .exec(CartScenario.addProductToCart)
-    .inject(rampUsers(100) during 3600)
+    .inject(rampUsers(5) during 180) //rampUsers(100) during 3600
 
   val scenario6  = scenario("Users: HP -> all product -> product details -> view cart")
     .exec(HPScenario.openHomePage)
     .exec(ProductScenario.viewAllProducts)
     .exec(ProductScenario.viewProductDetails)
     .exec(CartScenario.viewCart)
-    .inject(rampUsers(20) during 3600)
+    .inject(rampUsers(1) during 180) //rampUsers(20) during 3600
 
   val scenario7  = scenario("Users: HP -> all product -> product details -> view cart -> product details")
     .exec(HPScenario.openHomePage)
@@ -67,16 +68,14 @@ class LoadSimulation extends Simulation{
     .exec(ProductScenario.viewProductDetails)
     .exec(CartScenario.viewCart)
     .exec(ProductScenario.viewProductDetails)
-    .inject(rampUsers(160) during 3600)
+    .inject(rampUsers(8) during 180) //rampUsers(160) during 3600
 
   val scenario8  = scenario("Users: HP -> all product -> product details")
     .exec(HPScenario.openHomePage)
     .exec(ProductScenario.viewAllProducts)
     .exec(ProductScenario.viewProductDetails)
-    .inject(rampUsers(20) during 3600)
+    .inject(rampUsers(1) during 180) //rampUsers(20) during 3600
 
 
   setUp(scenario1,scenario2,scenario3,scenario4,scenario5,scenario6,scenario7,scenario8)
-
-
 }
